2984efd41f81baf1071a5e241a985ebd8d213e17
luizzmg <129332942+luizzmg@users.noreply.github.com>
2023-09-17 13:50:59 -0300

    colocando a parte dos coletáveis
    Signed-off-by: luizzmg <129332942+luizzmg@users.noreply.github.com>

--- Experimentos/vitor/main.py
+++ Experimentos/vitor/main.py
@@ -20,6 +20,24 @@ clock = pygame.time.Clock()
 
 map = Map(screen)
 
+# parte dos coletáveis
+
+branco = (255, 255, 255)
+preto = (0, 0, 0)
+vermelho = (255, 0, 0)
+verde = (0, 255, 0)
+azul = (0, 0, 255)
+
+class Coletavel:
+  def __init__(self, cor):
+    self.rect = pygame.Rect(random.choice([50, 200]), random.randint(50, 640-50), 30, 30)
+    self.cor = cor
+      
+pontuacoes = {vermelho: 0, verde: 0, azul: 0}
+coletaveis = [Coletavel(vermelho), Coletavel(verde), Coletavel(azul)]
+
+# parte do jogo rodando
+
 while game_running:
   screen.fill((0,0,0))      
 
@@ -29,3 +47,10 @@ while game_running:
 
   clock.tick(60)
   pygame.display.update()
+
+  for coletavel in coletaveis:
+    if player.rect.colliderect(coletavel.rect):
+  
+      pontuacoes[coletavel.cor] += 1
+      coletaveis.remove(coletavel)
+      coletaveis.append(Coletavel(coletavel.cor))
