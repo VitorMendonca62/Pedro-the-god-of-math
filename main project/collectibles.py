6272a13bdb1d609715e1453052af3b8c33704b46
VitorMendonca62 <vitorqueiroz325@gmail.com>
2023-09-20 13:58:14 -0300

    otimizando o codigo de luiz

--- main project/collectibles.py
+++ main project/collectibles.py
@@ -1,15 +1,35 @@
 import pygame
-from random import randint
-from level import levels_size
+import random
+from colors import *
+    # self.rect = pygame.draw.rect(screen,self.color,(x,colle_y,20,20))
 
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
-yellow = (255,255,0)
+class Collectible():
+  def __init__(self, item):
+    self.item = item
+    self.all_collectibles = list()
+    self.rect = pygame.Rect(0,0, 0, 0)
+    
+    self.row = 0
+    self.column = 0
 
-class Collectible:
-    def __init__(self, place, color, colle_x, colle_y,id):
-        self.rect = pygame.draw.rect(place,color,(colle_x,colle_y,20,20))
-        self.color = color
-        self.id = id
-        self.collected = False
+    if item == "r": self.color = red
+    if item == "g": self.color = green
+    if item == "b": self.color = blue
+    if item == "y": self.color = yellow
+
+    choice = self.take_choice(item)
+    self.row = random.choice(choice[0])
+    self.column = random.choice(choice[1])
+
+  def analyze_collision(self, player,matriz):
+    if player.colliderect(self.rect):
+      matriz[self.row][self.column] = matriz[self.row][self.column][:-1]
+    return player.colliderect(self.rect)
+
+  def take_choice(self, item):
+    interval0_6 = range(0,6)
+    interval7_14 =  range(7,14)
+    if item == "r": return [interval0_6, interval0_6]
+    if item == "g": return [interval7_14, interval0_6]
+    if item == "b": return [interval0_6, interval7_14]
+    if item == "y": return [interval7_14, interval7_14]
--- main project/map.py
+++ main project/map.py
@@ -1,15 +1,15 @@
 import pygame
 from level import Level
 from pygame.locals import *
+from collectibles import *
+from colors import *
 
 pygame.init()
 
-vertical_wall = pygame.image.load("assets/vertical-wall.png")
-horizontal_wall = pygame.image.load("assets/horizontal-wall.png")
-
 class Map():
   def __init__(self,screen):
-    self.all_collectibles = list() # todos os coletaveis
+    self.all_collectibles_rects = list() # todos os coletaveis
+    self.collectibles = list() # todos os coletaveis
     self.screen = screen
     self.walls_rects = list() # Vai estar armazenado todos os retangulos das paredes 
     # É no eixo das abscissas e ordenadas onde o mapa está localizado
@@ -20,55 +20,39 @@ class Map():
     self.last_y = 0
     self.size_map = 15 # tamanho do mapa
 
+    self.symbols_collectibles = ("r","g","b","y")
+
     level = Level()
     self.matriz_game = level.do_matriz_map() # Vai pegar a matriz do mapa
     self.draw_map(self.screen, self.x, self.y, True) # Vai desenhar o mapa
-    
-    # escolhendo os pontos a desenhar os coletáveis na tela
-    # em cada "quadrante" do labirinto vai ser possível o spawn de uma determinada cor de coletável
-    
-    # parte superor esquerda do labirinto com red
-    self.r_adress_x = random.choice([0, 6])
-    self.r_adress_y = random.choice([0, 6])
-    self.matriz_game[0][self.r_adress_x][self.r_adress_y] += "r"
-    
-    # parte superior direita do labirinto com green
-    self.g_adress_x = random.choice([7, 14])
-    self.g_adress_y = random.choice([0, 6])
-    self.matriz_game[0][self.g_adress_x][self.g_adress_y] += "g"
+     
+    self.collected = 0
 
-    # parte inferior esquerda do labirinto com blue
-    self.b_adress_x = random.choice([0, 6])
-    self.b_adress_y = random.choice([7, 14])
-    self.matriz_game[0][self.b_adress_x][self.b_adress_y] += "b"
+    for symbol in self.symbols_collectibles:
+      collectible = Collectible(symbol)
+      self.collectibles.append(collectible)
+      self.matriz_game[collectible.row][collectible.column] += symbol
 
-    # parte inferior direita do labirinto com yellow
-    self.y_adress_x = random.choice([7, 14])
-    self.y_adress_y = random.choice([7, 14])
-    self.matriz_game[0][self.y_adress_x][self.y_adress_y] += "y"
-
-    # fim da escolha dos pontos
-
-  def draw_map(self,screen,x,y,born): 
+  def draw_map(self, screen, x, y, born): 
     square_size = 60 # Tamanho do quadrado
     height_square_size = 4 # Tamanho do quadrado
 
-    for line in range(self.size_map):
+    for row in range(self.size_map):
       for column in range(self.size_map):
-        items = list(self.matriz_game[line][column]) # vai ser os simbolos de um elemento da matriz, por exemplo <^Sy
+        items = list(self.matriz_game[row][column]) # vai ser os simbolos de um elemento da matriz, por exemplo <^Sy
 
         for item in items:
           # indicar em qual coordenada o item vai ficar, é a partir do tamanho do quadrado e onde o item ta localizado na matriz e um valor dinamico x ou y, isso faz com que o mapa movimente a partir das teclas pressioandas
           item_x = square_size * column + x
-          item_y = square_size * line + y
+          item_y = square_size * row + y
 
           # desenhando as paredes
-          if item == ">":
-            item_x += 60
-          elif item == "v":
-            item_y += 60
-
           if item in ("<","^", ">", "v"):
+            if item == ">":
+              item_x += 60
+            elif item == "v":
+              item_y += 60
+
             if item == "<" or item == ">":
               rect = pygame.Rect(item_x, item_y, height_square_size, square_size)
             elif item == "^" or item == "v":
@@ -77,33 +61,22 @@ class Map():
             self.walls_rects.append(rect)
             pygame.draw.rect(screen, (255,255,255), rect)
 
-          
-          # desenhando coletáveis na tela dados os pontos escolhidos
-          red = (255, 0, 0)
-          green = (0, 255, 0)
-          blue = (0, 0, 255)
-          yellow = (255,255,0)
+          if item in self.symbols_collectibles:
+            item_x += 22
+            item_y += 22
+            size = 20
 
-          colle_x = wall_x + 22
-          colle_y = wall_y + 22
-
-          if direction == "r":
-            self.all_collectibles.append(Collectible(screen,red,colle_x,colle_y,"r"))
-
-          if direction == "g":
-            self.all_collectibles.append(Collectible(screen,green,colle_x,colle_y,"g"))
-
-          if direction == "b":
-            self.all_collectibles.append(Collectible(screen,blue,colle_x,colle_y,"b"))
-
-          if direction == "y":
-            self.all_collectibles.append(Collectible(screen,yellow,colle_x,colle_y,"y"))
-          # fim do desenho dos coletáveis
-
-
+            for collectible in self.collectibles:
+              if collectible.item == item:
+                rect = pygame.Rect(item_x,item_y, size, size)
+                pygame.draw.rect(screen, collectible.color, rect)
+                collectible.rect = rect
+                collectible.row = row
+                collectible.column = column
+            
           if item == "S" and born:
             self.x = - square_size * (column) + square_size * 4 
-            self.y = - square_size * (line / 2 + 2) - square_size / 2 
+            self.y = - square_size * (row / 2 + 2) - square_size / 2 
             
   def move_map(self):
     self.last_x = self.x
@@ -134,27 +107,13 @@ class Map():
         self.y = self.last_y
         self.pace = 0
         self.walls_rects = list()
-        
-    # analisando a colisão com os coletáveis
-    for colle in self.all_collectibles:
-      if player.colliderect(colle):
 
-        if colle.id == "r":
-          self.matriz_game[0][self.r_adress_x][self.r_adress_y] = self.matriz_game[0][self.r_adress_x][self.r_adress_y][:-1]
-          colle.collected = True
-
-        if colle.id == "g":
-          self.matriz_game[0][self.g_adress_x][self.g_adress_y] = self.matriz_game[0][self.g_adress_x][self.g_adress_y][:-1]
-          colle.collected = True
-
-        if colle.id == "b":
-          self.matriz_game[0][self.b_adress_x][self.b_adress_y] = self.matriz_game[0][self.b_adress_x][self.b_adress_y][:-1]
-          colle.collected = True
-
-        if colle.id == "y":
-          self.matriz_game[0][self.y_adress_x][self.y_adress_y] = self.matriz_game[0][self.y_adress_x][self.y_adress_y][:-1]
-          colle.collected = True
-    # fim da analise da colisão
+    for collectible in self.collectibles:
+      is_collision = collectible.analyze_collision(player, self.matriz_game)
+      if is_collision:
+        self.collectibles.remove(collectible)
+        self.collected += 1
+        print(self.collected)
 
   def update(self,player):
     self.analyze_collision(player)
