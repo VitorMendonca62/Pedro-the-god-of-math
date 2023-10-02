8fe179c80e2c7b608251c2201bfc6aeb93f8d62d
Luisgsm324 <luisf.moreira324@gmail.com>
2023-10-01 22:47:00 -0300

    Música no jogo

--- main project/main_luis.py
+++ main project/main_luis.py
@@ -138,7 +138,7 @@ def main_menu():
                 if START_BUTTON.clicked(mouse_pos):
                     pygame.mixer.Sound.play(CLICK_SOUND)
                     print("Clicado")
-                    pygame.mixer.music.load('./assets/sounds/Soundtrack_menu.mp3') # Música de Background 
+                    pygame.mixer.music.load('./assets/sounds/game_soundtrack.mp3') # Música de Background 
                     pygame.mixer.music.play(-1) # O -1 foi colcoado para estar em loop 
                     main_game()
                 if INSTRUCTIONS_BUTTON.clicked(mouse_pos):
